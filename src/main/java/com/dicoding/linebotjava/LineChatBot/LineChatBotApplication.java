ba5273b093b785e8989235e2cdd17f2fd154ef62
gdp.bts.03 <gdp.bts.03@gmail.com>
2020-03-20 07:50:47 +0700

    first commit

---
+++ src/main/java/com/dicoding/linebotjava/LineChatBot/LineChatBotApplication.java
@@ -0,0 +1,13 @@
+package com.dicoding.linebotjava.LineChatBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineChatBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineChatBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dicoding/linebotjava/LineChatBot/LineChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dicoding.linebotjava.LineChatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
