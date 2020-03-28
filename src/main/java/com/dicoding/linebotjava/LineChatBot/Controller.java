385aa066aa5d8c0ea9f33d5c47d2897502adcbfe
gdp.bts.03 <gdp.bts.03@gmail.com>
2020-03-28 15:56:52 +0700

    implementasi profile API

--- src/main/java/com/dicoding/linebotjava/LineChatBot/Controller.java
+++ src/main/java/com/dicoding/linebotjava/LineChatBot/Controller.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import sun.plugin.util.UserProfile;
 
 import java.io.IOException;
 import java.util.Arrays;
